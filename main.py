985566a69b09aa71c1d99904f9590a0b7d793502
Marcelo Brosowicz de Paulo <mbrosowicz@hotmail.com>
2018-04-22 17:27:21 -0300

    User search initial commit

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import time
 
 start_time = time.time()
 PROJECT_NAME = 'YouTube'
-URL = 'https://www.youtube.com/results?search_query=Chitãozinho+e+Chororó&sp=EgIQAUICCAE%253D'
+url = 'https://www.youtube.com/results?search_query={}&sp=EgIQAUICCAE%253D'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
 
 
 # Executa a Spider e o rastreamento da página
 def main():
-    Spider(PROJECT_NAME, URL)
+    search = input('Digite o que deseja buscar: ')
+    Spider(PROJECT_NAME, url.format(search))
 
 
 # Executa método main
